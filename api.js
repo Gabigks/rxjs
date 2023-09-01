5e97463ada2fe595ed6e28f3899b0e3d50401762
Gabigks <gabrielgks10@gmail.com>
2023-09-01 11:31:55 -0300

    Concurrency requests, a lot of ways to make paraleles request from a api

---
+++ api.js
@@ -0,0 +1,20 @@
+const http = require('http');
+
+http.createServer((req, res) => {
+  res.writeHead(200, {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+  });
+
+  const matchURL = /^\/response\/(.+)\/delay\/(\d+)\/?$/;
+  // http://localhost:5200/response/{"data": "Hello World"}/delay/1000/
+  if(!matchURL.test(req.url)) return res.end();
+
+  const [, response, delay] = matchURL.exec(req.url);
+  const jsonResponse = JSON.parse(decodeURIComponent(response));
+
+  setTimeout(() => {
+    res.write(jsonResponse);
+    res.end();
+  }, +delay)
+}).listen(5200);
